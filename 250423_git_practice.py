295291a97bd6f2437941ed5609ba3b415d8fc6a4
joonlee2614 <joonlee2614@gmail.com>
2025-04-23 01:25:37 +0900

    conf: Add settings for practicing python and git
    feat: Add Python script for basic multiple function for practicing

---
+++ 250423_git_practice.py
@@ -0,0 +1,10 @@
+class Multiplier:
+    def __init__(self, num1:int, num2:int):
+        self.result = num1 * num2
+
+
+mult = Multiplier (5,2)
+mult1 = Multiplier (8, 6)
+
+print(mult.result)
+print(mult1.result)
